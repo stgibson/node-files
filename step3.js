9e43f3b067df31216ed1d7c94b87f8adb51912cd
Sean Gibson <seanthomasgibson@gmail.com>
2021-01-03 13:44:30 -0800

    Handle multiple input files or urls.

--- step3.js
+++ step3.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const axios = require("axios");
 
 /**
- * Errors for when user types in wrong number of command-line arguments
+ * Errors for when user types in wrong number of command line arguments
  */
 class CommandArgsError extends Error {
   constructor(message) {
@@ -15,13 +15,14 @@ class CommandArgsError extends Error {
  * @param {string} data Text to add to filename
  * @param {string} filename Path to the file to write to
  */
-function writeToFile(data, filename) {
-  fs.writeFile(filename, data, "utf8", err => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-  });
+function appendToFile(data, filename) {
+  try {
+    fs.appendFileSync(filename, data, "utf8");
+  }
+  catch(err) {
+    console.log(err);
+    process.exit(1);
+  }
 }
 
 /**
@@ -30,20 +31,19 @@ function writeToFile(data, filename) {
  * @param {string} filename Path of file to write to
  */
 function cat(path, filename) {
-  fs.readFile(path, "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      process.exit(1);
+  try {
+    const data = fs.readFileSync(path, "utf8");
+    if (filename) {
+      appendToFile(data, filename);
     }
     else {
-      if (filename) {
-        writeToFile(data, filename);
-      }
-      else {
-        console.log(data);
-      }
+      console.log(data);
     }
-  });
+  }
+  catch(err) {
+    console.log(err);
+    process.exit(1);
+  }
 }
 
 /**
@@ -55,14 +55,14 @@ async function webCat(url, filename) {
   try {
     const { data } = await axios.get(url);
     if (filename) {
-      writeToFile(data, filename);
+      appendToFile(data, filename);
     }
     else {
       console.log(data);
     }
   }
-  catch(e) {
-    console.log(e);
+  catch(err) {
+    console.log(err);
     process.exit(1);
   }
 }
@@ -76,6 +76,7 @@ function isUrl(link) {
   return link.startsWith("http://") || link.startsWith("https://");
 }
 
+// determine which function to call based on command line arguments
 const args = process.argv;
 try {
   if (args.length < 3) {
@@ -84,29 +85,39 @@ try {
   if (args[2] === "--out") {
     if (args.length < 5) {
       const message =
-        "When using --out, you must provide both the output file and the input file or url"
+        "When using --out, you must provide both the output file and at least one input file or url"
       throw new CommandArgsError(message);
     }
-    const outputFile = args[3];
-    const link = args[4];
-    if (isUrl(link)) {
-      webCat(link, outputFile);
-    }
-    else {
-      cat(link, outputFile);
+    const [, , , outputFile, ...links] = args;
+    // make outputFile empty to prepare it for appending
+    fs.writeFile(outputFile, "", "utf8", err => {
+      if (err) {
+        console.log(err);
+        process.exit(1);
+      }
+    });
+    for (let link of links) {
+      if (isUrl(link)) {
+        webCat(link, outputFile);
+      }
+      else {
+        cat(link, outputFile);
+      }
     }
   }
   else {
-    const link = args[2];
-    if (isUrl(link)) {
-      webCat(link);
-    }
-    else {
-      cat(link);
+    const [, , ...links] = args;
+    for (let link of links) {
+      if (isUrl(link)) {
+        webCat(link);
+      }
+      else {
+        cat(link);
+      }
     }
   }
 }
-catch(e) {
-  console.log(e);
+catch(err) {
+  console.log(err);
   process.exit(1);
 }
